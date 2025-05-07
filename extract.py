7bff692d501ec7ccb37fa9bfda7a3f85de34572e
z21210 <z21210@protonmail.com>
2025-05-07 14:55:22 +0100

    Changed source to celestrak API

--- extract.py
+++ extract.py
@@ -1,21 +1,35 @@
 import requests as r
-import logging
 
-_sat_count = lambda j: j['parameters']['page-size'] * (j['parameters']['page'] - 1) + len(j['member'])
-_sat_total = lambda j: j['totalItems']
-_progress  = lambda j: f'{_sat_count(j)}/{_sat_total(j)}'
+def _tle2dict(name, l1, l2):
+	return {
+		'name':name,
+		'catalog_number':l1[2:7],
+		'classification':l1[7:8],
+		'launch_year':l1[9:11],
+		'launch_number':l1[11:14],
+		'piece':l1[14:17],
+		'year':l1[18:20],
+		'day':l1[20:32],
+		'velocity':l1[33:43],
+		'acceleration': l1[44:52],
+		'drag':l1[53:61],
+		'ephemeris':l1[62:63],
+		'set_number':l1[64:68],
+		'checksum1':l1[68:69],
+		'inclination':l2[8:16],
+		'right_ascension':l2[17:25],
+		'eccentricity':l2[26:33],
+		'periapsis_argument':l2[34:42],
+		'mean_anomaly':l2[43:51],
+		'revolution_rate':l2[52:63],
+		'revolutions':l2[63:68],
+		'checksum2':l2[68:69]
+	}
+
+_tles2dicts = lambda a: [_tle2dict(n,l1,l2) for n,l1,l2 in a]
 
 def fetch_sats():
-	logging.warning('Fetching satellite data from API (if this is running frequently, something is misconfigured)')
-	logging.info('This may take some time...')
-	# get first page
-	page = r.get('https://tle.ivanstanojevic.me/api/tle?page-size=100', headers={"User-Agent":"Mozilla/5.0"})
-	sats = page.json()['member']
-	logging.info(_progress(page.json()))
-	while 'next' in page.json()['view']:
-		# get next page
-		page = r.get(page.json()['view']['next'], headers={"User-Agent":"Mozilla/5.0"})
-		sats.extend(page.json()['member'])
-		logging.info(_progress(page.json()))
-	return sats
-
+	resp = r.get("https://celestrak.org/NORAD/elements/gp.php?GROUP=active")
+	lines = resp.text.split('\r\n')
+	tles = [lines[i:i+3] for i in range(0, len(lines)-1, 3)]
+	return _tles2dicts(tles)
--- transform.py
+++ transform.py
@@ -1,10 +1,32 @@
-from sgp4.api import Satrec, SatrecArray, jday
-from time import gmtime
+import pandas as pd
 
-_gmt2jday = lambda t: jday(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
-_json2sat = lambda j: Satrec.twoline2rv(j['line1'], j['line2'])
+# a curse upon NORAD for this format
+# may a thousand years of drive failures befall you
+_non_standard_form2float = lambda s: (-1 if s[0]=='-' else 1) * float('.'+s[1:6]) *10**int(s[6:8])
 
-def transform(jsons):
-	return SatrecArray([
-		_json2sat(j)
-	for j in jsons])
+def transform(df):
+	# strip whitespace from name
+	df['name'] = df['name'].str.strip()
+	# last two digits of year; range is 1957 to 2056: y=1957+(y-57)%100
+	df['launch_year'] = df['launch_year'].astype(int).sub(57).mod(100).radd(1957)
+	# unusual number format
+	df['acceleration'] = df['acceleration'].map(_non_standard_form2float)
+	df['drag'] = df['drag'].map(_non_standard_form2float)
+	# assumed decimal point
+	df['eccentricity'] = df['eccentricity'].radd('.').astype(float)
+	# trivial casts
+	df = df.astype({
+		'catalog_number':int,
+		'launch_number':int,
+		'year':int,
+		'day':float,
+		'velocity':float,
+		'set_number':int,
+		'checksum1':int,
+		'right_ascension':float,
+		'periapsis_argument':float,
+		'mean_anomaly':float,
+		'revolution_rate':float,
+		'revolutions':int,
+		'checksum2':int
+	})
