4f39294331359dc3317f1c608e66354aea81ead8
z21210 <z21210@protonmail.com>
2025-05-13 13:01:54 +0100

    changed tabs to radio and added live updates

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlalchemy as sa
 import os
 import env
+import time
 
 from astropy import units as u
 from astropy.time import Time
@@ -16,48 +17,88 @@ from hapsira.plotting import OrbitPlotter
 from hapsira.earth.plotting import GroundtrackPlotter
 from sgp4.api import Satrec
 
-# load data from database
-db, schema, table = os.getenv('DB'), os.getenv('SCHEMA'), os.getenv('TABLE')
-df = pd.read_sql_table(table, sa.create_engine(db), schema=schema, index_col='catalog_number')
-# create Satrecs
-df['satrec'] = df.apply(lambda s: Satrec.twoline2rv(*s[['l1','l2']]), axis=1)
-# create plotters
-gp = GroundtrackPlotter()
-op = OrbitPlotter(backend=Plotly3D())
+@st.cache_data
+def fetch_satellite_data():
+    # load data from database
+    db, schema, table = os.getenv('DB'), os.getenv('SCHEMA'), os.getenv('TABLE')
+    df = pd.read_sql_table(table, sa.create_engine(db), schema=schema, index_col='catalog_number')
+    return df
+df = fetch_satellite_data()
 
 # UI layout
 with st.sidebar:
-    st.write("Select Satellites")
+    st.write('Select Satellites')
+    view = st.radio(
+        'View',
+        ['Groundtrack', 'Orbit', 'Data']
+    )
     selected = st.multiselect(
-        "Select Satellites",
+        'Search Satellites',
         df.index,
         format_func=lambda i: df.loc[i]['name'],
         default=[25544], # ISS
     )
-tabGT, tabO = st.tabs(["Groundtrack", "Orbit"])
-tabGT.plotly_chart(gp.fig)
-tabO.plotly_chart(op.backend.figure)
-
-# plot selected satellites
-now = Time.now()
-jd, fr = now.jd1, now.jd2
-for s in selected:
-    e, r, v = df.loc[s]['satrec'].sgp4(jd, fr)
-    if e != 0:
-        continue
-    orbit = Orbit.from_vectors(Earth, r<<u.km, v<<(u.km/u.s), epoch=now)
-
-    gp.plot(
-        EarthSatellite(orbit, None),
-        time_range(
-            now-1.5*u.h, num_values=150, end=now+1.5*u.h
-        ),
-        label=df.loc[s]['name'],
-        color="blue",
-        marker={
-            "size": 10,
-            "symbol": "triangle-right",
-            "line": {"width": 1, "color": "black"},
-        },
+    live = st.toggle(
+        'Live view',
+        disabled= view=='Data',
+        help='Plots will be re-rendered each interval'
     )
-    op.plot(orbit)
+    interval = st.number_input(
+        'Refresh interval (s)',
+        disabled= view=='Data' or not live,
+        min_value=5,
+        max_value=600,
+        value=10
+    )
+plot = st.empty()
+
+
+# create Satrecs
+df['satrec'] = df.apply(lambda s: Satrec.twoline2rv(*s[['l1','l2']]), axis=1)
+
+
+# if data view, render once
+if view == 'Data':
+    pass
+# if plot view, render once or forever
+else:
+    while True:
+        plot.empty()
+        gp = GroundtrackPlotter()
+        op = OrbitPlotter(backend=Plotly3D())
+        now = Time.now()
+        jd, fr = now.jd1, now.jd2
+        for s in selected:
+            e, r, v = df.loc[s]['satrec'].sgp4(jd, fr)
+            if e != 0:
+                continue
+            orbit = Orbit.from_vectors(Earth, r<<u.km, v<<(u.km/u.s), epoch=now)
+            # groundtrack
+            if view == 'Groundtrack':
+                gp.plot(
+                    EarthSatellite(orbit, None),
+                    time_range(
+                        now-1.5*u.h, num_values=150, end=now+1.5*u.h
+                    ),
+                    label=df.loc[s]['name'],
+                    color='blue',
+                    marker={
+                        'size': 10,
+                        'symbol': 'triangle-right',
+                        'line': {'width': 1, 'color': 'black'},
+                    },
+                )
+                with plot.container():
+                    st.plotly_chart(gp.fig)
+            # orbit
+            elif view == 'Orbit':
+                op.plot(
+                    orbit,
+                    label=df.loc[s]['name']
+                )
+                with plot.container():
+                    st.plotly_chart(op.backend.figure)
+        # break or sleep before next loop
+        if not live:
+            break
+        time.sleep(interval)
