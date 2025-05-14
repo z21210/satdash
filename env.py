1d3aed03dc052f689ecf4fa86623bde468090380
z21210 <z21210@protonmail.com>
2025-05-14 18:33:17 +0100

    removed launch argument requirement

--- env.py
+++ env.py
@@ -4,12 +4,10 @@ import os
 import errno
 
 try:
-    env = sys.argv[1]
-    assert load_dotenv(f'.env.{env}', override=True)
+    if len(sys.argv) > 1:
+        env = sys.argv[1]
+        load_dotenv(f'.env.{env}', override=True)
     assert all(e is not None for e in [os.getenv('DB'), os.getenv('SCHEMA'), os.getenv('TABLE')])
-except IndexError:
-    print('You must specify environment')
-    sys.exit(errno.EINVAL)
 except AssertionError:
     print(f'You must provide .env.{env} with DB, SCHEMA, and TABLE')
     sys.exit(errno.EINVAL)
